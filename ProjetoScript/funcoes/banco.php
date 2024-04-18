e618f50d8d4674df39cc53bcf5042bd587cea542
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-04-18 10:43:28 -0300

    update

---
+++ ProjetoScript/funcoes/banco.php
@@ -0,0 +1,13 @@
+<<?php
+
+function conectar()
+{
+  $user = "root";
+  $pass = "";
+  $dsn = "mysql:host=localhost;dbname=escola";
+  $connection = new PDO($dsn, $user, $pass);
+  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $connection;
+}
+
+?>
