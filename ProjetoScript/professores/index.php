1f2064ed7127e07cadb115b6618dc406aa2d1b22
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-04-18 11:29:50 -0300

    update

---
+++ ProjetoScript/professores/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<?php
+include_once "../funcoes/banco.php";
+$bd = conectar();
+$consulta = "SELECT * FROM professores order by nome";
+$resultado = $bd->query($consulta);
+?>
+
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Projeto Script</title>
+</head>
+
+<body>
+
+  <a href="novo.php"><button>Novo Professor</button></a>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Professor</th>
+        <th>Ações</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      while ($prof = $resultado->fetch()) {
+        echo "<tr>";
+        echo "<td>";
+        echo $prof['Nome'];
+        echo "</td>";
+        echo "<td>";
+        echo " Editar | Consultar | Excluir ";
+        echo "</td>";
+        echo "</tr>";
+      }
+      $resultado = null;
+      $bd = null;
+      ?>
+    </tbody>
+  </table>
+
+</body>
+
+</html>
